fbd434b7db0717e4dc499188155a97a10856f97b
justin <jmartin9@lhup.edu>
2015-02-23 14:09:58 -0500

    Forgot the Physics Class

---
+++ TilD/src/utils/Physics.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.awt.Rectangle;
+
+import main.Enemy;
+import main.GameObject;
+import main.Player;
+
+public class Physics {
+
+	public static boolean collides(GameObject player, GameObject enemy) {
+		Rectangle playerRect = new Rectangle((int) player.getX(),
+				(int) player.getY(), player.getWidth(), player.getHeight());
+		Rectangle enemyRect = new Rectangle((int) enemy.getX(),
+				(int) enemy.getY(), enemy.getWidth(), enemy.getHeight());
+		if (playerRect.intersects(enemyRect)) {
+			return true;
+		} else {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	}
+
+}
